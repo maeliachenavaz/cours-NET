12e293f443083d29ad79875608ad56cbb6658099
a-Kaiko <maelia.chenavaz@hotmail.com>
2025-03-24 14:18:53 +0100

    condition - étape 2

--- ConsoleApp1/Programme.cs
+++ ConsoleApp1/Programme.cs
@@ -7,14 +7,27 @@
         Console.WriteLine("Taux d'imposition : ");
         int tauxImposition = InputToInt(Console.ReadLine());
 
-        if (salaireAnnuelBrut == 0 && tauxImposition == 0)
+        if (salaireAnnuelBrut == 0 || tauxImposition == 0)
         {
             Console.WriteLine("Dommage pour vous..");
         } else
         {
-            float salaireAnnuelNet = CalculSalaireMensuelNet(salaireAnnuelBrut, tauxImposition);
+            float salaireMensuelNet = CalculSalaireMensuelNet(salaireAnnuelBrut, tauxImposition);
 
-            Console.WriteLine("Salaire net mensuel : " + salaireAnnuelNet);
+            if (salaireAnnuelBrut > 50000)
+            {
+                Console.WriteLine("Vous devriez faire un don pour réduire vos impôts.");
+            } else if (salaireAnnuelBrut >= 30000 && salaireAnnuelBrut <= 40000)
+            {
+                Console.WriteLine("Viens au CESI pour un bac +5 en dév !");
+            }
+
+            if (CalculSalaireMensuelBrut(salaireAnnuelBrut) < 1500)
+            {
+                Console.WriteLine("C'est normal pour un alternant c:");
+            }
+
+            Console.WriteLine("Salaire mensuel net : " + salaireMensuelNet);
         }
     }
 
@@ -23,6 +36,7 @@
         try
         {
             int inputToInt = Convert.ToInt32(input);
+
             return inputToInt;
         } catch (System.FormatException) {
             return 0;
@@ -32,15 +46,24 @@
     private static float CalculSalaireMensuelNet(int salaireAnnuelBrut, int tauxImposition) 
     {
         float salaireMensuelBrut = CalculSalaireMensuelBrut(salaireAnnuelBrut);
-        float taxeImpostion = salaireMensuelBrut * (tauxImposition/100);
+        float taxeImpostion = CalculTaxeImposition(salaireMensuelBrut, tauxImposition);
 
         float salaireAfterTaxe = salaireMensuelBrut - taxeImpostion;
+
         return salaireAfterTaxe;
+    }
+
+    private static float CalculTaxeImposition (float salaireMensuelBrut ,int tauxImposition)
+    {
+        float taxeImpostion = salaireMensuelBrut * (tauxImposition / 100);
+
+        return taxeImpostion;
     }
 
     private static float CalculSalaireMensuelBrut(int salaireAnnuelBrut)
     {
         float salaireMensuelBrut = salaireAnnuelBrut / 12;
+
         return salaireMensuelBrut;
     }
 }
