69515991fa900433c3fe8c82b51c4bc25908243d
a-Kaiko <maelia.chenavaz@hotmail.com>
2025-03-24 11:31:29 +0100

    étape 1

---
+++ ConsoleApp1/Programme.cs
@@ -0,0 +1,16 @@
+﻿class Programme
+{
+    static void Main(string[] args)
+    {
+        int salaire = 30000;
+
+        float mensuel = calculMensuel(salaire);
+        Console.WriteLine(args[0] + " mensuel : " + mensuel);
+    }
+
+    static float calculMensuel(int salaire)
+    {
+        float mensuel = salaire / 12;
+        return mensuel;
+    }
+}
