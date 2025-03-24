629e0f17fe613da4a3883eeffcd985c5dc9de8ba
a-Kaiko <maelia.chenavaz@hotmail.com>
2025-03-24 11:45:18 +0100

    étape 2

--- ConsoleApp1/Programme.cs
+++ ConsoleApp1/Programme.cs
@@ -2,13 +2,24 @@
 {
     static void Main(string[] args)
     {
-        int salaire = 30000;
+        int salaire = Convert.ToInt32(args[0]);
+        int taxe = Convert.ToInt32(args[1]);
 
-        float mensuel = calculMensuel(salaire);
-        Console.WriteLine(args[0] + " mensuel : " + mensuel);
+        float salaireNet = calculSalaireMensuelNet(salaire, taxe);
+        Console.WriteLine("Salaire annuel brut : " + salaire);
+        Console.WriteLine("Taxe : " + taxe);
+        Console.WriteLine("Salaire net mensuel : " + salaireNet);
     }
 
-    static float calculMensuel(int salaire)
+    static float calculSalaireMensuelNet(int salaire, int taxe) 
+    {
+        float salaireMensuel = calculSalaireMensuelBrut(salaire);
+        float tauxImposition = taxe / 100;
+        float salaireAfterTaxe = salaireMensuel - (salaireMensuel * tauxImposition);
+        return salaireAfterTaxe;
+    }
+
+    static float calculSalaireMensuelBrut(int salaire)
     {
         float mensuel = salaire / 12;
         return mensuel;
