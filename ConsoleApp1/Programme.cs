a800ecf50ed0f3680efaf87d963c6de67f0924ca
a-Kaiko <maelia.chenavaz@hotmail.com>
2025-03-25 10:00:18 +0100

    class partie 3

--- ConsoleApp1/Programme.cs
+++ ConsoleApp1/Programme.cs
@@ -2,8 +2,50 @@
 {
     public static void Main()
     {
-        User user = new User(1, "Bob", "Bricoleur", 24, 30000, 15, 10);
-        user.PrintUserInfo();
-        user.PrintSalaireParMois();
+        Console.WriteLine("1- Afficher ses informations");
+        Console.WriteLine("2- Calculer ses intérêts composés");
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        if (input == 1)
+        {
+            Console.Clear();
+            User user = new User(1, "Bob", "Bricoleur", 24, 30000, 15, 10);
+            user.PrintUserInfo();
+            Console.WriteLine("");
+            user.PrintSalaireParMois();
+        }
+        else if (input == 2)
+        {
+            Console.Clear();
+            Console.WriteLine("Montant investit ? ");
+            int montantInvestit = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A quel taux ? ");
+            int taux = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sur combien d'années ? ");
+            int annee= Convert.ToInt32(Console.ReadLine());
+
+            PrintInteretCompose(montantInvestit, taux, annee);
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Faire un choix dans ceux proposés.");
+            Main();
+        }
+    }
+
+    private static void PrintInteretCompose(int montantInvestit, int taux, int annee)
+    {
+        double tauxInvestissement = taux / 100.0;
+        double montantActuel = montantInvestit;
+        for (int i = 0; i < annee; i ++)
+        {
+            double gainAjoute = montantActuel * tauxInvestissement;
+            double montantTotal = montantActuel + gainAjoute;
+            Console.WriteLine("Annee " + i + " : " + montantTotal);
+            montantActuel = montantTotal;
+        }
     }
 }
